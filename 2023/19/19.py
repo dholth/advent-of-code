82888486217cfc1656370c41b49b72578b115248
Daniel Holth <dholth@anaconda.com>
2023-12-19 19:37:32 -0500

    19 part 2

--- 2023/19/19.py
+++ 2023/19/19.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
-import aocd
-from itertools import groupby
-from rich.console import Console
+import math
 import re
+from itertools import groupby
+
+import aocd
+import networkx as nx
+from rich.console import Console
 
 print = Console().print
 
@@ -36,7 +39,10 @@ hdj{m>838:A,pv}
 """.splitlines()
 
 
-def splitblank(lines):
+def sections(lines):
+    """
+    Split lines by blank lines.
+    """
     return [[*g] for k, g in groupby(lines, bool) if k]
 
 
@@ -52,15 +58,15 @@ def parse_ops(ops):
         yield (source, [m.groupdict() for m in inner_pattern.finditer(program)])
 
 
-data = aocd.data.splitlines()
-flows, items = splitblank(data)
+full_data = aocd.data.splitlines()
+flows, items = sections(full_data)
 
 items = list(parse_items(items))
 
 flows = dict(parse_ops(flows))
 
-print(items)
-print(flows)
+# print(items)
+# print(flows)
 
 
 def interpret_one(item, flow):
@@ -109,3 +115,112 @@ def score(items):
 
 
 print("Score", score(accepted))
+
+
+START = 1
+END = 4001
+XMAS = "xmas"
+
+
+def show(graph):
+    import matplotlib.pyplot as plt
+
+    subax1 = plt.subplot()
+    pos = nx.shell_layout(graph)
+    nx.draw(graph, pos=pos, with_labels=True, ax=subax1)
+
+    plt.show()
+
+
+def display_part2(data):
+    flows = dict(parse_ops(sections(data)[0]))
+
+    digraph = nx.DiGraph()
+    for k, v in flows.items():
+        for step in v:
+            digraph.add_edge(k, step["next"])
+
+    try:
+        nx.cycles.find_cycle(digraph)
+        raise ValueError("Graph has cycles")
+    except nx.exception.NetworkXNoCycle:
+        print("Graph has no cycles")
+
+    show(digraph)
+
+
+# display_part2(aocd.data.splitlines())
+
+
+def format_step(step):
+    if step["op"]:
+        return f"{step['var']}{step['op']}{step['val']}"
+    return "."
+
+
+def inverse_step(step):
+    match step["op"]:
+        case None:
+            return "."
+        case ">":
+            return {**step, "op": "<="}
+        case "<":
+            return {**step, "op": ">="}
+        case _:
+            raise ValueError("Unexpected")
+
+
+def step_to_range(step):
+    r = None
+    if op := step["op"]:
+        value = int(step["val"])
+        match op:
+            case ">":
+                r = range(value + 1, END)
+            case "<":
+                return range(START, value)
+            case ">=":
+                return range(value, END)
+            case "<=":
+                return range(START, value + 1)
+            case _:
+                raise ValueError("Unexpected")
+    return r
+
+
+def merge_ranges(r1: range, r2: range):
+    return range(max(r1.start, r2.start), min(r1.stop, r2.stop))
+
+
+def steps_to_ranges(steps):
+    xmas = dict(zip("xmas", (range(START, END),) * 4))
+    for step in steps:
+        r = step_to_range(step)
+        if r is not None:
+            var = step["var"]
+            xmas[var] = merge_ranges(xmas[var], r)
+    return xmas
+
+
+def traverse_graph(flows, here, previous=()):
+    if here in "RA":
+        # print(list(format_step(s) for s in previous), here)
+        if here == "A":
+            yield steps_to_ranges(previous)
+        return
+
+    for step in flows[here]:
+        yield from traverse_graph(flows, step["next"], previous + (step,))
+        # and inverse of step
+        previous = previous + (inverse_step(step),)
+
+
+flows = dict(parse_ops(sections(full_data)[0]))
+traversals = list(traverse_graph(flows, "in"))
+print(f"Found {len(traversals)} accepted paths")
+
+all = 0
+for t in traversals:
+    all += sum([math.prod(len(r) for r in t.values())])
+
+print("Accepted", all)

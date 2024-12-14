ccf6cd4fa3cc39db3ffa70cd0477f49fab1d8c69
Daniel Holth <dholth@anaconda.com>
2024-12-14 09:26:53 -0500

    day 14

---
+++ 2024/14/14.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+import operator
+import re
+import time
+from collections import defaultdict
+from dataclasses import dataclass
+from functools import reduce
+
+import aocd
+
+FILLER = "\uff03"
+PRETTY = {
+    " ": "\N{FULLWIDTH FULL STOP}",
+    ".": "\N{FULLWIDTH FULL STOP}",
+    "#": "🧱",
+    "R": "🤖",
+}
+
+
+class V(tuple):
+    def __new__(cls, *args):
+        return super(V, cls).__new__(cls, args)
+
+    def __add__(self, other):
+        return self.__class__(*(a + b for a, b in zip(self, other)))
+
+    def __sub__(self, other):
+        return self.__class__(*(a - b for a, b in zip(self, other)))
+
+    def __mul__(self, scale):
+        return self.__class__(*(a * scale for a in self))
+
+    def __mod__(self, other):
+        return self.__class__(*(a % b for a, b in zip(self, other)))
+
+    def taxi(self):
+        a, b = self
+        return int(abs(a) + abs(b))
+
+    def __repr__(self):
+        return f"V{str((*self,))}"
+
+
+@dataclass
+class Robot:
+    p: V
+    v: V
+
+
+def parse(data: str):
+    for line in data.splitlines():
+        a, b, c, d = map(int, re.findall(r"-?\d+", line))
+        yield Robot(p=V(a, b), v=V(c, d))
+
+
+def display(robots, size: tuple, show_quadrants=False):
+    grid = {robot.p: robot for robot in robots}
+    width, height = size
+
+    midpoint = size[0] // 2, size[1] // 2
+
+    for y in range(height):
+        row = []
+        for x in range(width):
+            if (x, y) in grid:
+                row.append("\N{ROBOT FACE}")
+            else:
+                # row.append("\N{FULLWIDTH FULL STOP}")
+                row.append("  ")  # double space as fullwidth
+
+            if show_quadrants and (x == midpoint[0] or y == midpoint[1]):
+                row[-1] = PRETTY.get("#")
+
+        print("".join(row))
+
+
+def by_quadrant(robots, size: tuple):
+    width, height = size
+    midpoint = width // 2, height // 2
+    mw, mh = midpoint
+
+    by_quadrant = defaultdict(list)
+
+    for robot in robots:
+        x, y = robot.p
+        quadrant = (x > mw) - (x < mw), (y > mh) - (y < mh)
+        if not any(c == 0 for c in quadrant):
+            by_quadrant[quadrant].append(robot)
+
+    return dict(by_quadrant)
+
+
+def triangularity(q):
+    # express many more on one side of the chart
+
+    # V(17, 7) (-1, -1) Top Left
+    # V(92, 46) (1, -1) Top Right
+    # (-1, 1) Bottom left
+    # (1, 1) Bottom right
+
+    tl = len(q[(-1, -1)])
+    tr = len(q[(1, -1)])
+    bl = len(q[(-1, 1)])
+    br = len(q[(1, 1)])
+
+    top_bottom = abs((tl + tr) - (bl + br))
+
+    left_right = abs((tl + bl) - (tr + br))
+
+    return max(top_bottom, left_right)
+
+
+example = """\
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+"""
+
+robots = list(parse(example))
+
+
+def simulate(robots: list[Robot], seconds: int, size: tuple):
+    for robot in robots:
+        robot.p = (robot.p + robot.v * seconds) % size
+
+
+print("Initial state")
+display(robots, (11, 7))
+
+simulate(robots, 100, (11, 7))
+
+print("After 100 seconds")
+display(robots, (11, 7), show_quadrants=True)
+
+print(
+    "Example",
+    reduce(operator.mul, (len(v) for v in by_quadrant(robots, (11, 7)).values())),
+)
+
+full_size = (101, 103)
+robots = list(parse(aocd.data))
+assert len(robots) == len(aocd.data.splitlines())
+simulate(robots, 100, full_size)
+display(robots, full_size, show_quadrants=True)
+
+print(
+    "Part 1",
+    reduce(operator.mul, (len(v) for v in by_quadrant(robots, full_size).values())),
+)
+
+## Part 2 (look for christmas tree)
+
+
+def count_top_left(robots):
+    return sum((1 < robot.p[0] < 32 and 1 < robot.p[1] < 32) for robot in robots)
+
+
+robots = list(parse(aocd.data))
+max_tri = 0
+for i in range(1, 6476):
+    simulate(robots, 1, full_size)
+    tri = triangularity(by_quadrant(robots, full_size))
+    if tri > (max_tri * 0.94):  # tri == 252 at goal
+        # print("\033[2J\033[H")  # clear screen and move to home
+        max_tri = max(tri, max_tri)
+        display(robots, full_size)
+        print(f"{i} seconds elapsed\n")
+        print("Triangularity", tri, "Max", max_tri, "Ratio", tri / max_tri)
+        time.sleep(0.1)
+
+    if i % 2000 == 0:
+        print(f"{i} seconds")
